1d5ea65878ecfb6512e34ecadd841a5e11cc9760
kickscar <kickscar@gmail.com>
2022-05-03 08:30:55 +0900

    filterchain docs #6 DelegatingFilterProxy 추가

--- 05.springsecurity/01.filter/src/test/java/me/kickscar/spring/security/web/FilterChainTest.java
+++ 05.springsecurity/01.filter/src/test/java/me/kickscar/spring/security/web/FilterChainTest.java
@@ -10,6 +10,7 @@ import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.cookie;
@@ -30,7 +31,7 @@ public class FilterChainTest {
     @BeforeEach
     public void setup() {
         mvc = MockMvcBuilders
-                .webAppContextSetup(context).addFilter(filterChainManager, "/*")
+                .webAppContextSetup(context).addFilter(new DelegatingFilterProxy(filterChainManager), "/*")
                 .build();
     }
 
